a72825748d6ae0439ad56817d0cd08cf0871c0c5
Sam Mckay <sammckay12@gmail.com>
2018-12-03 12:11:04 +0000

    removing files for production

--- determine_users.py
+++ determine_users.py
@@ -1,19 +1,30 @@
 import numpy as np
 import json
 # import mapping_id
-import virtual_mapping
+import mapping_id
 
 
 with open('./data/locations.json') as locations:
     locations_data = json.load(locations)
 
-def find_location_id(ranked_items):
-    for num in ranked_items[:15]:
-        for attr, value in virtual_mapping.LOCATIONS.items():
+def find_location_id(ranked_items, scores):
+    results = []
+    for num in ranked_items:
+        place_list = ranked_items.tolist()
+        scores_list = scores.tolist()
+        for attr, value in mapping_id.LOCATIONS.items():
             if value == num:
-                find_location_name(attr)
-                print(attr, value)
+                results.append({"place_id" : attr, "rank": place_list.index(num), "score": scores_list[num]})
+    return results
 
 def find_location_name(id):
     place = next((x for x in locations_data if x['_id'] == id), None)
-    print(place['name'])
+    return place
+
+def find_user_id(kompas_user_id):
+    print(kompas_user_id)
+    for attr, value in mapping_id.USERS.items():
+        if attr == kompas_user_id:
+            return value
+        break
+    return value
